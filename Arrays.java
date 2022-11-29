6e410b10e5d8a69e04b2e70b038f458d69dd0b82
kevinkaruitha <118059935+kevinkaruitha@users.noreply.github.com>
2022-11-29 23:14:54 +0300

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Arrays {
+
+   
+    public static void main(String[] args) {
+        Scanner in;
+        in = newScanner(S;.
+        ystem.in);
+        System.out.println("input the fisrt number:");
+        int a=in.nextInt();
+        System.out.println("input the second number:");
+        int b=in.nextInt();
+        System.out.println("results;+results(a,b):");
+        
+    }
+
+    private static int result(int x,u) {
+        
+        
+    }
+
+    private static Scanner newScanner(InputStream in) {
+        if(x==u)
+            return 0;
+        if(x%6==y6)
+            return(x<u)?x;u;
+            return(x>u)?x;u;
+    }
+            
+    }
+    
+}
